348b3715ad1d245ca344cd041255e7c85d8302c4
Ray Weitzenberg <ray@mechaneyes.com>
2020-06-30 17:32:40 -0400

    lines drawing at random Ys
    class has been tightened up. now drawing 5 lines with random y values.
    right now they're all moving in sync

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -10,48 +10,50 @@ export default function sketch(s) {
 
   // const yPos = 10
   let lineOne
+  let theLines = []
 
   s.setup = () => {
     s.createCanvas(600, 600);
     backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
     s.frameRate(30);
-    actStrokeCap = s.ROUND;
 
 
-    for (let ranY = 1; ranY <= 5; ranY++) {
-      console.log("ranIncr", ranY)
-      // let randomPos = random()
-      lineOne = new singleLine(x1, x2, s.pow(2, ranY))
+    for (let ranY = 0; ranY < 5; ranY++) {
+      theLines.push(new singleLine())
+      // console.log('theLines', theLines[ranY])
+      // console.log('ranY', ranY)
     }
   };
 
   s.draw = () => {
     s.clear();
     s.background(242, 203, 5);
-    
-    lineOne.moveLine()
+
+    for (let i = 0; i < theLines.length; i++) {
+      theLines[i].move()
+      theLines[i].display()
+    }
   };
 
   class singleLine {
     constructor(x1, x2, yPos) {
       this.x1 = x1;
       this.x2 = x2;
-      this.yPos = yPos;
+      this.x1 = 0;
+      this.x2 = 50;
+      this.yPos = s.random(s.height);
     }
 
-    moveLine() {
-      // s.line(this.x1, s.height / 2, this.x2, s.height / 2);
-      s.line(this.x1, this.yPos, this.x2, this.yPos);
-
-      console.log('this.yPos', this.yPos)
-
+    move() {
       if (this.x2 < s.width) {
         this.x2 += velocity;
+        console.log('this.x2', this.x2)
       }
 
-      if (this.x2 === s.width) {
+      if (this.x2 >= s.width) {
         this.x2 = s.width;
         this.x1 += velocity;
+        console.log('x2 end', this.x2)
       }
 
       if (this.x1 >= s.width) {
@@ -59,16 +61,9 @@ export default function sketch(s) {
         this.x2 = 0;
       }
     }
+
+    display() {
+      s.line(this.x1, this.yPos, this.x2, this.yPos);
+    }
   }
-
-  s.mousePressed = () => {
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
-    actRandomSeed = s.random(100000);
-  };
-
-  s.keyReleased = () => {
-    if (s.key == "1") actStrokeCap = s.ROUND;
-    if (s.key == "2") actStrokeCap = s.SQUARE;
-    if (s.key == "3") actStrokeCap = s.PROJECT;
-  };
 }
