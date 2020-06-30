99a78c1969fef5dffeb91ea65114fd3aeb0a1548
Ray Weitzenberg <ray@mechaneyes.com>
2020-06-30 19:46:36 -0400

    added topBottomLines
    equal number of lines streaming top to bottom to contrast left to right

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -1,6 +1,7 @@
 export default function sketch(s) {
   
-  let theLines = []
+  let leftRightLines = []
+  let topBottomLines = []
 
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
@@ -9,7 +10,8 @@ export default function sketch(s) {
 
 
     for (let ranY = 0; ranY < 70; ranY++) {
-      theLines.push(new leftRightLine())
+      leftRightLines.push(new leftRightLine())
+      topBottomLines.push(new topBotLine())
     }
   };
 
@@ -17,10 +19,12 @@ export default function sketch(s) {
     s.clear();
     s.background(242, 203, 5);
 
-    for (let i = 0; i < theLines.length; i++) {
-      theLines[i].move(s.random(30, 50))
-      theLines[i].display()
-      // console.log('theLines[0]', theLines[0])
+    for (let i = 0; i < leftRightLines.length; i++) {
+      leftRightLines[i].move(s.random(30, 50))
+      leftRightLines[i].display()
+      // console.log('leftRightLines[0]', leftRightLines[0])
+      topBottomLines[i].move(s.random(30, 50))
+      topBottomLines[i].display()
     }
   };
 
@@ -58,34 +62,33 @@ export default function sketch(s) {
   }
 
   class topBotLine {
-    constructor(x1, x2, yPos) {
-      this.y1 = x1;
-      this.y2 = x2;
+    constructor() {
       this.y1 = 0;
       this.y2 = s.random(-400, s.height);
-      this.xPos = s.random(s.height);
+      this.xPos = s.random(s.width);
+
       this.velocity = s.random(20, 50)
     }
 
-    move(randVelocity) {
-      if (this.x2 < s.width) {
-        this.x2 += randVelocity;
+    move() {
+      if (this.y2 < s.height) {
+        this.y2 += this.velocity;
       }
 
-      if (this.x2 >= s.width) {
-        this.x2 = s.width;
-        this.x1 += randVelocity;
+      if (this.y2 >= s.height) {
+        this.y2 = s.height;
+        this.y1 += this.velocity;
       }
 
-      if (this.x1 >= s.width) {
-        this.x1 = 0;
-        this.x2 = 0;
+      if (this.y1 >= s.height) {
+        this.y1 = 0;
+        this.y2 = 0;
         this.yPos = s.random(s.height);
       }
     }
 
     display() {
-      s.line(this.x1, this.yPos, this.x2, this.yPos);
+      s.line(this.xPos, this.y1, this.xPos, this.y2);
     }
   }
 }
