68ef85bebc8c83f707af84308cc0d33083838666
Ray Weitzenberg <ray@mechaneyes.com>
2020-07-02 12:32:36 -0400

    leftToRight, rightToLeft topToBottom
    have 3 classes spanwning elements going in these three directions. the
    number elements spawned is based on the size of the viewport

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -1,18 +1,25 @@
 export default function sketch(s) {
-  
-  let leftRightLines = []
-  let topBottomLines = []
+  let topBottomLines = [];
+  let leftRightLines = [];
+  let rightLeftLines = [];
 
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
-    // backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
-    // s.frameRate(30);
 
-    const numLines = s.windowWidth / 20
+    // Number of spanwned elements is based on viewport size
+    //
+    const numVertLines = s.windowWidth / 25;
+    console.log("numVertLines", numVertLines);
+    const numHorizLines = s.windowHeight / 70;
+    console.log("numHorizLines", numHorizLines);
 
-    for (let ranY = 0; ranY < numLines; ranY++) {
-      leftRightLines.push(new leftRightLine())
-      topBottomLines.push(new topBotLine())
+    for (let ranY = 0; ranY < numVertLines; ranY++) {
+      topBottomLines.push(new topBotLine());
+    }
+
+    for (let ranY = 0; ranY < numHorizLines; ranY++) {
+      leftRightLines.push(new leftRightLine());
+      rightLeftLines.push(new rightLeftLine());
     }
   };
 
@@ -20,27 +27,29 @@ export default function sketch(s) {
     s.clear();
     s.background(242, 203, 5);
 
+    for (let i = 0; i < topBottomLines.length; i++) {
+      topBottomLines[i].move(s.random(30, 50));
+      topBottomLines[i].display();
+    }
+
     for (let i = 0; i < leftRightLines.length; i++) {
-      leftRightLines[i].move(s.random(30, 50))
-      leftRightLines[i].display()
-      // console.log('leftRightLines[0]', leftRightLines[0])
-      topBottomLines[i].move(s.random(30, 50))
-      topBottomLines[i].display()
+      leftRightLines[i].move(s.random(30, 50));
+      leftRightLines[i].display();
+
+      rightLeftLines[i].move(s.random(30, 50));
+      rightLeftLines[i].display();
     }
   };
 
   class leftRightLine {
     constructor(x1, x2, yPos) {
-      this.x1 = x1;
-      this.x2 = x2;
       this.x1 = 0;
       this.x2 = s.random(10, s.width);
       this.yPos = s.random(s.height);
-      this.velocity = s.random(20, 100)
+      this.velocity = s.random(20, 100);
     }
 
     move(randVelocity) {
-
       if (this.x2 < s.width) {
         this.x2 += this.velocity;
       }
@@ -62,13 +71,43 @@ export default function sketch(s) {
     }
   }
 
+  class rightLeftLine {
+    constructor(x1, x2, yPos) {
+      this.x1 = s.width;
+      this.x2 = s.width;
+      this.yPos = s.random(s.height);
+      this.velocity = s.random(20, 100);
+    }
+
+    move(randVelocity) {
+      if (this.x1 > 0) {
+        this.x1 -= this.velocity;
+      }
+
+      if (this.x1 <= 0) {
+        this.x1 = 0;
+        this.x2 -= this.velocity;
+      }
+
+      if (this.x2 <= 0) {
+        this.x1 = s.width;
+        this.x2 = s.width;
+        this.yPos = s.random(s.height);
+      }
+    }
+
+    display() {
+      s.line(this.x1, this.yPos, this.x2, this.yPos);
+    }
+  }
+
   class topBotLine {
     constructor() {
       this.y1 = 0;
       this.y2 = s.random(-400, s.height);
       this.xPos = s.random(s.width);
 
-      this.velocity = s.random(20, 50)
+      this.velocity = s.random(20, 50);
     }
 
     move() {
